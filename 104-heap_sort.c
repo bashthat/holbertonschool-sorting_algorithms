93d7d7d9d085b7f21e8bf675601f23e8d83c0a2e
bashthat <3249@holbertonschool.com>
2022-09-11 16:33:28 -0500

    editing

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,87 +1,87 @@
 #include "sort.h"
-/**
- * sort - sorts an array of integers in ascending order
- * @a: array to sort
- * @b: size of array
- */
-
-
-void sort(int *a, int b)
-{
-    int x, y;
-    int z;
-
-    for (x = 0; x < b; x++)
-    {
-        for (y = 0; y < b - x - 1; y++)
-        {
-            if (a[y] > a[y + 1])
-            {
-                exchange(&a[y], &a[y + 1]);
-                print_array(a, b);
-            }
-        }
-    }
-        }
-
-
 
 /**
- * exchange - exchanges two values
- * @x: first value
- * @y: second value
+ * merge - merge two sorted arrays
+ * @array: first array
+ * @temp: temporary array
+ * @size: size of array
  */
-
-void exchange(int *x, int *y)
+void exchange(int *a, int *b)
 {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
- * heapify - heapifies an array
- * @a: array to heapify
- * @b: size of array
- * @c: index of array
+ * merge - merge two sorted arrays
+ * @array: first array
+ * @temp: temporary array
+ * @size: size of array
  */
-
-void heapify(int *a, int b, int c)
+void sort(int *a, int n)
 {
-    size_t largest = c;
-    size_t left = 2 * c + 1;
-    size_t right = 2 * c + 2;
+	int i, j;
+	int temp;
 
-    if (left < b && a[left] > a[largest])
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = 0; j < n - i - 1; j++)
+		{
+			if (a[j] > a[j + 1])
+			{
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+}
+
+/**
+ * merge - merge two sorted arrays
+ * @array: first array
+ * @temp: temporary array
+ * @size: size of array
+ */
+void heapify(int *a, size_t b, size_t c)
+{
+    size_t largest = b;
+    size_t left = 2 * b + 1;
+    size_t right = 2 * b + 2;
+
+    if (left < c && a[left] > a[largest])
         largest = left;
-    if (right < b && a[right] > a[largest])
+
+    if (right < c && a[right] > a[largest])
         largest = right;
-    if (largest != c)
+
+    if (largest != b)
     {
-        exchange(&a[c], &a[largest]);
-        print_array(a, b);
-        heapify(a, b, largest);
+        exchange(&a[b], &a[largest]);
+        heapify(a, largest, c);
     }
 }
+
 /**
- * heap_sort - sorts an array of integers in ascending order
+ * heap_sort - sort an array using heap sort
  * @array: array to sort
  * @size: size of array
  */
+void heap_sort(int *array, size_t size)
+{
+	int xyz;
+	size_t x = size;
 
-void heap_sort(int *array, size_t size) {
-    int z;
-    size_t i = size;
+	for (xyz = x / 2 - 1; xyz >= 0; xyz--)
+	{
+		heapify(array, x, xyz);
+	}
 
-    for (z = size / 2 - 1; z >= 0; z--)
-    {
-        heapify(array, size, z);
-    }
-
-    for (z = size - 1; z >= 0; z--)
-    {
-        exchange(&array[0], &array[z]);
-        print_array(array, size);
-        heapify(array, z, 0);
-    }
+	for (xyz = x - 1; xyz > 0; xyz--)
+	{
+		swap(&array[0], &array[xyz]);
+		x--;
+		heapify(array, x, 0);
+	}
 }
--- sort.h
+++ sort.h
@@ -37,7 +37,7 @@ void merge(int *array, int *left, int *right, size_t size);
 void heap_sort(int *array, size_t size);
 void exchange(int *x, int *y);
 void sort(int *a, int b);
-void heapify(int *a, int b, int c);
+void heapify(int *a, size_t b, size_t c);
 
 
 
