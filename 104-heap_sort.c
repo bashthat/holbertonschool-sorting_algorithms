63207638c307b42b88f007d2ff59c4adafd02432
Sam <97474996+bashthat@users.noreply.github.com>
2022-09-11 16:43:06 -0500

    results

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,87 +1,55 @@
 #include "sort.h"
-
 /**
- * merge - merge two sorted arrays
- * @array: first array
- * @temp: temporary array
- * @size: size of array
- */
-void exchange(int *a, int *b)
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-/**
- * merge - merge two sorted arrays
- * @array: first array
- * @temp: temporary array
- * @size: size of array
- */
-void sort(int *a, int n)
-{
-	int i, j;
-	int temp;
-
-	for (i = 0; i < n - 1; i++)
-	{
-		for (j = 0; j < n - i - 1; j++)
-		{
-			if (a[j] > a[j + 1])
-			{
-				temp = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = temp;
-			}
-		}
-	}
-}
-
-/**
- * merge - merge two sorted arrays
- * @array: first array
- * @temp: temporary array
- * @size: size of array
- */
-void heapify(int *a, size_t b, size_t c)
-{
-    size_t largest = b;
-    size_t left = 2 * b + 1;
-    size_t right = 2 * b + 2;
-
-    if (left < c && a[left] > a[largest])
-        largest = left;
-
-    if (right < c && a[right] > a[largest])
-        largest = right;
-
-    if (largest != b)
-    {
-        exchange(&a[b], &a[largest]);
-        heapify(a, largest, c);
-    }
-}
-
-/**
- * heap_sort - sort an array using heap sort
+ * heap_sort - sorts an array of integers in ascending order using the Heap sort
+ * algorithm
  * @array: array to sort
  * @size: size of array
  */
+
 void heap_sort(int *array, size_t size)
 {
-	int xyz;
-	size_t x = size;
-
-	for (xyz = x / 2 - 1; xyz >= 0; xyz--)
-	{
-		heapify(array, x, xyz);
-	}
-
-	for (xyz = x - 1; xyz > 0; xyz--)
-	{
-		exchange(&array[0], &array[xyz]);
-		x--;
-		heapify(array, x, 0);
-	}
+    size_t i, j, k, l;
+    int temp;
+    if (array == NULL || size < 2)
+        return;
+    for (i = size / 2; i > 0; i--)
+    {
+        for (j = i; j <= size / 2;)
+        {
+            k = j * 2;
+            if (k < size && array[k - 1] < array[k])
+                k++;
+            if (array[j - 1] < array[k - 1])
+            {
+                temp = array[j - 1];
+                array[j - 1] = array[k - 1];
+                array[k - 1] = temp;
+                j = k;
+            }
+            else
+                break;
+        }
+    }
+    for (l = size; l > 1; l--)
+    {
+        temp = array[0];
+        array[0] = array[l - 1];
+        array[l - 1] = temp;
+        print_array(array, size);
+        for (j = 1; j <= l / 2;)
+        {
+            k = j * 2;
+            if (k < l && array[k - 1] < array[k])
+                k++;
+            if (array[j - 1] < array[k - 1])
+            {
+                temp = array[j - 1];
+                array[j - 1] = array[k - 1];
+                array[k - 1] = temp;
+                j = k;
+            }
+            else
+                break;
+        }
+    }
 }
