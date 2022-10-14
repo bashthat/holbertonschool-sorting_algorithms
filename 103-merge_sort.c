d3a13c713809cef10983059761d39685e2020eeb
bashthat <3249@holbertonschool.com>
2022-10-14 15:41:04 -0500

    editing prototype

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -9,37 +9,17 @@
  * @temp: temp array
  */
  
- void merge(int *array, int *left, int *right, size_t size); {
- 	int i = 0, j = 0, k = 0;
- 	int n1 = mid - low + 1;
- 	int n2 = high - mid;
- 	int left[n1], right[n2];
- 	for (i = 0; i < n1; i++)
- 		left[i] = array[low + i];
- 	for (j = 0; j < n2; j++)
- 		right[j] = array[mid + 1 + j];
- 	i = 0;
- 	j = 0;
- 	k = low;
- 	while (i < n1 && j < n2) {
- 		if (left[i] <= right[j]) {
- 			array[k] = left[i];
- 			i++;
- 		} else {
- 			array[k] = right[j];
- 			j++;
- 		}
- 		k++;
- 	}
- 	while (i < n1) {
- 		array[k] = left[i];
- 		i++;
- 		k++;
- 	}
- 	while (j < n2) {
- 		array[k] = right[j];
- 		j++;
- 		k++;
- 	}
- 
- }
+void merge_sort(int *array, size_t size)
+{
+int *temp;
+size_t i;
+if (size < 2)
+return;
+temp = malloc(sizeof(int) * size);
+if (temp == NULL)
+return;
+for (i = 0; i < size; i++)
+temp[i] = array[i];
+merge(array, temp, 0, size - 1);
+free(temp);
+}
